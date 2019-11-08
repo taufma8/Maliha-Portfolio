cd135ef63ed4f0c8a9eddaaae605c10189c23e66
Maliha Taufiq <maliha.taufiq@verizon.com>
2019-11-08 09:19:11 -0600

    Initial Commit

---
+++ src/resumeData.js
@@ -0,0 +1,113 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/taufma8",
+    "name": "Maliha Taufiq",
+    "role": "Full Stack JavaScript Developer",
+    "linkedinId":"https://www.linkedin.com/in/maliha-taufiq-17737762/",
+    // "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/maliha-taufiq-17737762/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/taufma8",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am working at Verizon with the Operations Automation team. Currently, working on an application for network management.",
+    "education":[
+      {
+        "UniversityName":"The LNM Insitute of Information Technology",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "id": 1,
+        "project_name": "A Random Quote Generator",
+        "description": "A program that displays a randomly selected quote each time a user clicks the button.",
+        "technologies": ["JavaScript", "HTML", "CSS"],
+        "live_link": "https://taufma8.github.io/Project1/",
+        "github_link": "https://github.com/taufma8/Project1",
+        "image_urls": ["images/Project1.PNG", "/static/images/Project1-2.PNG"]
+    },
+    {
+        "id": 2,
+        "project_name": "Pagination and Filtering",
+        "description": "Created a list of people and used filtering to only show a certain number of people per page.",
+        "technologies": ["JavaScript", "HTML", "CSS"],
+        "live_link": "https://taufma8.github.io/Project2/",
+        "github_link": "https://github.com/taufma8/Project2",
+        "image_urls": ["images/Project2.PNG", "/static/images/Project2-2.PNG"]
+    },
+    {
+        "id": 3,
+        "project_name": "Interactive Form",
+        "description": "Created an interactive form for conference registration.",
+        "technologies": ["JavaScript", "HTML", "CSS"],
+        "live_link": "https://taufma8.github.io/Project3/",
+        "github_link": "https://github.com/taufma8/Project3",
+        "image_urls": ["images/Project3.PNG", "/static/images/Project3-2.PNG"]
+    },
+    {
+        "id": 4,
+        "project_name": "Game Show App",
+        "description": "A word guessing game for a random phrase.",
+        "technologies": ["JavaScript", "HTML", "CSS", "OOP"],
+        "live_link": "https://taufma8.github.io/Project-4/",
+        "github_link": "https://github.com/taufma8/Project-4",
+        "image_urls": ["images/Project4.PNG", "/static/images/Project4-2.PNG"]
+    },
+    {
+        "id": 5,
+        "project_name": "Employee Directory",
+        "description": "An employee directory by communicating with a third-party API.",
+        "technologies": ["JavaScript", "HTML", "CSS", "API"],
+        "live_link": "https://taufma8.github.io/Project5/",
+        "github_link": "https://github.com/taufma8/Project5",
+        "image_urls": ["images/Project5.PNG", "/static/images/Project5-2.PNG"]
+    }
+    ],
+  }
+  
+  export default resumeData
